d9d2a6fd0b362b1132fdeecf1407e0519d46d6f3
Rodolfo Bandeira <phpbr@yahoo.ca>
2014-11-27 06:14:04 -0500

    Foundation + translations pt_BR
    closes #3

--- install.sh
+++ install.sh
@@ -1,7 +1,17 @@
 #!/bin/sh
 
+echo "Setando acl no seu linux!"
+sudo apt-get install acl
 HTTPDUSER=`ps aux | grep -E '[a]pache|[h]ttpd|[_]www|[w]ww-data|[n]ginx' | grep -v root | head -1 | cut -d\  -f1`
 sudo setfacl -R -m u:"$HTTPDUSER":rwX -m u:`whoami`:rwX app/cache app/logs
 sudo setfacl -dR -m u:"$HTTPDUSER":rwX -m u:`whoami`:rwX app/cache app/logs
-
 echo "Permissoes setadas!"
+
+
+echo "Download do composer... "
+curl -s http://getcomposer.org/installer | php
+echo "Instalando os pacotes do composer.json na pasta vendors..."
+php composer.phar install
+echo "Pasta vendors instalada!"
+
+
