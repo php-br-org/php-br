a9f750957b56bb7056e32db87b79a997240020a5
root <contato@rodolfobandeira.com>
2015-01-01 08:42:21 -0500

    Fixed FOSUserBundle

--- src/Phpbr/Bundle/AppBundle/Controller/DefaultController.php
+++ src/Phpbr/Bundle/AppBundle/Controller/DefaultController.php
@@ -6,13 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
-    public function indexAction($name)
-    {
-        return $this->render('PhpbrAppBundle:Default:index.html.twig', array('name' => $name));
-    }
-
     public function quemsomosAction()
     {
         return $this->render('PhpbrAppBundle::quemsomos.html.twig');
+    }
+
+    public function inicialAction()
+    {
+        return $this->render('PhpbrAppBundle::inicial.html.twig');
     }
 }
