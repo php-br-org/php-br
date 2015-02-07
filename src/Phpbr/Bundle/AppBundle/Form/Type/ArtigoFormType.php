b6e7a66d23e8fca3a901193a04df00c40c20a4c7
Rodolfo Bandeira <contato@rodolfobandeira.com>
2015-02-07 10:37:12 -0500

    Tentativa de habilitar o TextAreaCountdownBundle

--- src/Phpbr/Bundle/AppBundle/Form/Type/ArtigoFormType.php
+++ src/Phpbr/Bundle/AppBundle/Form/Type/ArtigoFormType.php
@@ -5,12 +5,15 @@ namespace Phpbr\Bundle\AppBundle\Form\Type;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use BSky\Bundle\TextAreaCountdownBundle\Form\Extension\Type;
 
 class ArtigoFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options) {
         $builder
             ->add('titulo', null, array('attr' => array('class' => ''), 'required' => true))
+            // Como habilito o textareacountdown aqui? 
+            // ->add('resumo', 'textareacountdown', array('attr' => array('class' => '', 'rows' => 5), 'required' => false))
             ->add('resumo', 'textarea', array('attr' => array('class' => '', 'rows' => 5), 'required' => false))
             ->add('texto', 'textarea', array('attr' => array('class' => '', 'rows' => 20), 'required' => true))
             ->add('tags', null, array('attr' => array('class' => ''), 'required' => false))
