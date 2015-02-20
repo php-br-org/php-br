b560296990d593b616084b84e9a4e27b577afffb
Rodrigo Z A <rodrigo.zane-arthuso@nurun.com>
2015-02-20 15:39:22 -0500

    Adicionando highlight.js (incompleto)

--- src/Phpbr/Bundle/AppBundle/Resources/public/assets/js/app.js
+++ src/Phpbr/Bundle/AppBundle/Resources/public/assets/js/app.js
@@ -1,1 +1,16 @@
-$(document).foundation();
+//( function ( $ ) {
+    'use strict';
+
+    var phpbr = phpbr || {};
+
+    phpbr.prototype = {
+        highlight: function() {
+            hljs.initHighlightingOnLoad();
+        }
+    };
+
+    $(document).ready(function() {
+        $(document).foundation();
+        $(document).confirmWithReveal();
+    });
+//} ( jQuery ) );
