b4a6003db86b5068eeb65ad0b0a2f53aa4c3f3d6
Rodolfo Bandeira <contato@rodolfobandeira.com>
2015-02-24 22:51:54 -0300

    Add na entity User (Twitter, Linkedin, Github)

--- src/Phpbr/Bundle/AppBundle/Entity/User.php
+++ src/Phpbr/Bundle/AppBundle/Entity/User.php
@@ -23,6 +23,21 @@ class User extends BaseUser
     /**
      * @var string
      */
+    protected $linkedin;
+
+    /**
+     * @var string
+     */
+    protected $twitter;
+
+    /**
+     * @var string
+     */
+    protected $github;
+
+    /**
+     * @var string
+     */
     protected $facebook_id;
 
     /**
@@ -74,6 +89,79 @@ class User extends BaseUser
     {
         return $this->name;
     }
+
+    /**
+     * Set twitter
+     *
+     * @param string $twitter
+     * @return User
+     */
+    public function setTwitter($twitter)
+    {
+        $this->twitter = $twitter;
+
+        return $this;
+    }
+
+    /**
+     * Get twitter
+     *
+     * @return string
+     */
+    public function getTwitter()
+    {
+        return $this->twitter;
+    }
+
+    /**
+     * Set linkedin
+     *
+     * @param string $linkedin
+     * @return User
+     */
+    public function setLinkedin($linkedin)
+    {
+        $this->linkedin = $linkedin;
+
+        return $this;
+    }
+
+
+
+    /**
+     * Get linkedin
+     *
+     * @return string
+     */
+    public function getLinkedin()
+    {
+        return $this->linkedin;
+    }
+
+
+    /**
+     * Set github
+     *
+     * @param string $github
+     * @return User
+     */
+    public function setGithub($github)
+    {
+        $this->github = $github;
+
+        return $this;
+    }
+
+    /**
+     * Get github
+     *
+     * @return string
+     */
+    public function getGithub()
+    {
+        return $this->github;
+    }
+
 
     /**
      * Set facebook_id
