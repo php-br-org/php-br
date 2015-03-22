971319199726b1548eb6eae5e7d2c0700f767685
Rodolfo Bandeira <contato@rodolfobandeira.com>
2015-03-22 19:40:25 -0300

    mais uns pedacos do forum

--- src/Phpbr/Bundle/AppBundle/Controller/Forum/TopicoController.php
+++ src/Phpbr/Bundle/AppBundle/Controller/Forum/TopicoController.php
@@ -5,6 +5,7 @@ namespace Phpbr\Bundle\AppBundle\Controller\Forum;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Phpbr\Bundle\AppBundle\Entity\Forum\Topico;
+use Phpbr\Bundle\AppBundle\Entity\Forum\Categoria;
 use Phpbr\Bundle\AppBundle\Form\Type\ForumTopicoFormType;
 
 
@@ -12,15 +13,35 @@ class TopicoController extends Controller
 {
     public function indexAction()
     {
+        $em = $this->getDoctrine()->getManager();
+        $categorias = $em->getRepository('PhpbrAppBundle:Forum\Categoria')->findAll();
+
         return $this->render('PhpbrAppBundle:Forum:index.html.twig', array(
-                // ...
-            ));    }
+            'categorias' => $categorias
+        ));
+    }
 
     public function verAction($id)
     {
+        $em = $this->getDoctrine()->getManager();
+        $topicos = $em->getRepository('PhpbrAppBundle:Forum\Topico')
+            ->find($id);
+
         return $this->render('PhpbrAppBundle:Forum:ver.html.twig', array(
-                // ...
-            ));    }
+
+        ));
+    }
+
+    public function verTopicoAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $topicos = $em->getRepository('PhpbrAppBundle:Forum\Topico')
+            ->find($id);
+
+        return $this->render('PhpbrAppBundle:Forum:ver.html.twig', array(
+            'topicos' => $topicos
+        ));
+    }
 
     public function novoAction(Request $request)
     {
