a8c66bdf32031192f5c79c004d6414c466c9296c
Rodrigo Z A <rodrigo@sktboard.com>
2015-03-24 22:31:11 -0400

    [2] Atualizacao .gitignore

---
+++ install.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+echo "Setando acl no seu linux!"
+sudo apt-get install acl
+HTTPDUSER=`ps aux | grep -E '[a]pache|[h]ttpd|[_]www|[w]ww-data|[n]ginx' | grep -v root | head -1 | cut -d\  -f1`
+sudo setfacl -R -m u:"$HTTPDUSER":rwX -m u:`whoami`:rwX app/cache app/logs
+sudo setfacl -dR -m u:"$HTTPDUSER":rwX -m u:`whoami`:rwX app/cache app/logs
+echo "Permissoes setadas!"
+
+
+echo "Download do composer... "
+curl -s http://getcomposer.org/installer | php
+echo "Instalando os pacotes do composer.json na pasta vendors..."
+php composer.phar install
+echo "Pasta vendors instalada!"
+
+echo "Instalando and Dump para Assets: "
+php app/console assets:install web
+php app/console assetic:dump web
+echo "ok!! "
+
+echo "Removendo cache..."
+rm -rf app/cache/*
+php app/console cache:clear -e prod 
+echo "ok!! "
+
+echo "Configurando BD..."
+#php app/console doctrine:database:create
+php app/console doctrine:schema:create
+echo "ok!!"
+
+echo "Carregando DataFixtures"
+php app/console doctrine:fixtures:load
+echo "ok!!"
+
