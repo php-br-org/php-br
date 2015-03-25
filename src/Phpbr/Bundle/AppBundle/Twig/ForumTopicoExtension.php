fae692e07bf7872b7cd4c07098c01516205da99c
Rodolfo Bandeira <contato@rodolfobandeira.com>
2015-03-25 09:13:21 -0300

    Contar mensagens e posts + inicial + translations

---
+++ src/Phpbr/Bundle/AppBundle/Twig/ForumTopicoExtension.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Phpbr\Bundle\AppBundle\Twig;
+
+use Doctrine\ORM\EntityManager;
+
+class ForumTopicoExtension extends \Twig_Extension {
+
+    protected $em;
+
+    public function __construct(EntityManager $em)
+    {
+        $this->em = $em;
+    }
+
+    public function getFilters() {
+        return array(
+            new \Twig_SimpleFilter(
+                'quantidade_topicos',
+                array (
+                    $this,
+                    'getQuantidadeTopicos'
+                )
+            )
+        );
+    }
+
+    public function getQuantidadeTopicos($categoria_id) {
+        $topicos = $this->em->getRepository('PhpbrAppBundle:Forum\Topico')
+            ->findBy(
+                array('categoria' => $categoria_id)
+            );
+
+        return count($topicos);
+    }
+
+    public function getName() {
+        return 'quantidade_topicos';
+    }
+}
+
