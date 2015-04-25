093667bcfa3a8cdfda39e7eed8c44b0a17f1bd41
rodolfobandeira <contato@rodolfobandeira.com>
2015-04-25 05:17:08 -0400

    Add <tab> no textarea do cole + fix Travis-ci

--- src/Phpbr/Bundle/AppBundle/Resources/public/assets/js/app.js
+++ src/Phpbr/Bundle/AppBundle/Resources/public/assets/js/app.js
@@ -57,3 +57,20 @@ $(document).ready(function() {
 });
 
 
+$(document).delegate('#phpbr_bundle_appbundle_cole_codigo', 'keydown', function(e) {
+    var keyCode = e.keyCode || e.which;
+
+    if (keyCode == 9) {
+        e.preventDefault();
+        var start = $(this).get(0).selectionStart;
+        var end = $(this).get(0).selectionEnd;
+
+        $(this).val($(this).val().substring(0, start)
+        + "\t"
+        + $(this).val().substring(end));
+
+        $(this).get(0).selectionStart =
+            $(this).get(0).selectionEnd = start + 1;
+    }
+});
+
