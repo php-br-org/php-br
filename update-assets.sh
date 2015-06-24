083fcb56bb6160d0a1d0b8750e27bb015be5f12a
rodolfobandeira <contato@rodolfobandeira.com>
2015-06-24 08:19:57 -0400

    Algumas modificacoes no CSS lateral

---
+++ update-assets.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+###################################################################
+#
+# src/Phpbr/Bundle/AppBundle/Resources/public/assets
+#
+###################################################################
+
+echo 'Compiling SCSS';
+cd src/Phpbr/Bundle/AppBundle/Resources/public/assets
+grunt build
+
+cd ../../../../../../..
+
+
+echo 'Removing css and js';
+rm -rf web/css/*
+rm -rf web/js/*
+
+
+echo 'Dumping and Installing assets';
+php app/console assetic:dump
+php app/console assets:install
